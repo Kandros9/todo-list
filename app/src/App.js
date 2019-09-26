95b7a529614b1af83f1e341ad26d36520d7a87e7
evelina_r <evelinka-r@mail.ru>
2019-09-26 20:21:30 +0300

    checklist added

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
+import {Button, Checkbox, Input} from "antd";
+import 'antd/dist/antd.css';
 
 function App() {
-  return (
-    <div>
-        ToDo App
-    </div>
-  );
+    const todos = [3, 4, 5, 6, 7, 8, 9, 10];
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.inputContainer}>
+                    <Input/>
+                    <Button type="primary" className={styles.actionButton}>+ Add</Button>
+                </div>
+                {todos.map(() => <TodoItem/>)}
+            </div>
+        </div>
+    );
+}
+
+function TodoItem() {
+    return (
+        <div className={styles.listContainer}>
+            <div className={styles.listItem}>
+                <Checkbox/>
+                <h2 className={styles.todoTitle}>Buy milk</h2>
+            </div>
+        </div>
+    );
+
 }
 
 export default App;
